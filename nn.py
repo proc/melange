9f1211d66c308938525c64e921544657867b8a57
Peter Tran <proc@m1snack.local>
2022-01-20 18:07:58 -0800

    create project

---
+++ nn.py
@@ -0,0 +1,90 @@
+import torch
+from torch import nn
+from torch.utils.data import DataLoader
+from torchvision import datasets
+from torchvision.transforms import ToTensor, Lambda, Compose
+import matplotlib.pyplot as plt
+
+# Download training data from open datasets.
+training_data = datasets.FashionMNIST(
+    root="data",
+    train=True,
+    download=True,
+    transform=ToTensor(),
+)
+
+# Download test data from open datasets.
+test_data = datasets.FashionMNIST(
+    root="data",
+    train=False,
+    download=True,
+    transform=ToTensor(),
+)
+
+batch_size = 64
+
+# Create data loaders.
+train_dataloader = DataLoader(training_data, batch_size=batch_size)
+test_dataloader = DataLoader(test_data, batch_size=batch_size)
+
+# for X, y in test_dataloader:
+#     print("Shape of X [N, C, H, W]: ", X.shape)
+#     print("Shape of y: ", y.shape, y.dtype)
+#     break
+
+# Get cpu or gpu device for training.
+device = "cuda" if torch.cuda.is_available() else "cpu"
+print(f"Using {device} device")
+
+# Define model
+class NeuralNetwork(nn.Module):
+    def __init__(self):
+        super(NeuralNetwork, self).__init__()
+        self.flatten = nn.Flatten()
+        self.linear_relu_stack = nn.Sequential(
+            nn.Linear(28*28, 512),
+            nn.ReLU(),
+            nn.Linear(512, 512),
+            nn.ReLU(),
+            nn.Linear(512, 10)
+        )
+
+    def forward(self, x):
+        x = self.flatten(x)
+        logits = self.linear_relu_stack(x)
+        return logits
+
+model = NeuralNetwork()
+model.load_state_dict(torch.load("./models/model.pth"))
+
+classes = [
+    "T-shirt/top",
+    "Trouser",
+    "Pullover",
+    "Dress",
+    "Coat",
+    "Sandal",
+    "Shirt",
+    "Sneaker",
+    "Bag",
+    "Ankle boot",
+]
+
+model.eval()
+x, y = test_data[0][0], test_data[0][1]
+with torch.no_grad():
+    pred = model(x)
+    predicted, actual = classes[pred[0].argmax(0)], classes[y]
+    print(f'Predicted: "{predicted}", Actual: "{actual}"')
+
+x, y = test_data[1][0], test_data[1][1]
+with torch.no_grad():
+    pred = model(x)
+    predicted, actual = classes[pred[0].argmax(0)], classes[y]
+    print(f'Predicted: "{predicted}", Actual: "{actual}"')
+
+x, y = test_data[2][0], test_data[2][1]
+with torch.no_grad():
+    pred = model(x)
+    predicted, actual = classes[pred[0].argmax(0)], classes[y]
+    print(f'Predicted: "{predicted}", Actual: "{actual}"')
---
+++ stock.py
@@ -0,0 +1,472 @@
+
+import torch
+from torch import nn
+from torch.utils.data import DataLoader
+from torchvision import datasets
+from torchvision.transforms import ToTensor, Lambda, Compose
+import matplotlib.pyplot as plt
+from sklearn.preprocessing import MinMaxScaler
+import numpy as np
+import pandas as pd
+
+filepath = 'data/TSLA15m.csv'
+data = pd.read_csv(filepath)
+data = data.sort_values('Timestamp')
+data.head()
+
+import plotly.graph_objs as go
+from plotly.offline import plot
+
+device = "cuda" if torch.cuda.is_available() else "cpu"
+print(f"{device}" " is available.")
+
+def plot_dataset(df, title):
+    data = []
+
+    value = go.Scatter(
+        x=df.index,
+        y=df['Close'],
+        mode="lines",
+        name="values",
+        marker=dict(),
+        text=df.index,
+        line=dict(color="rgba(0,0,0, 0.3)"),
+    )
+    data.append(value)
+
+    layout = dict(
+        title=title,
+        xaxis=dict(title="Timestamp", ticklen=5, zeroline=False),
+        yaxis=dict(title="Close Price (USD)", ticklen=5, zeroline=False),
+    )
+
+    fig = dict(data=data, layout=layout)
+    plot(fig)
+
+df = data.set_index(['Timestamp'])
+df.index = pd.to_datetime(df.index)
+
+if not df.index.is_monotonic:
+    df = df.sort_index()
+
+plot_dataset(df, title='TSLA Close Price')
+
+def generate_time_lags(df, n_lags):
+    df_n = df.copy()
+    for n in range(1, n_lags + 1):
+        df_n[f"lag{n}"] = df_n["Close"].shift(n)
+    df_n = df_n.iloc[n_lags:]
+    return df_n
+
+input_dims = 100
+
+df_generated = generate_time_lags(df, input_dims)
+df_generated
+print(df_generated)
+
+df_features = (
+    df
+    .assign(hour = df.index.hour)
+    .assign(day = df.index.day)
+    .assign(month = df.index.month)
+    .assign(day_of_week = df.index.dayofweek)
+    .assign(week_of_year = df.index.week)
+    # .assign(open_price = df['Open'])
+    # .assign(close_price = df['Close'])
+    # .assign(high_price = df['High'])
+    # .assign(low_price = df['Low'])
+    # .assign(name = df['Name'])
+)
+
+print(df_features)
+
+
+from sklearn.model_selection import train_test_split
+
+def feature_label_split(df, target_col):
+    print(target_col)
+
+    y = df[[target_col]]
+    x = df.drop(columns=[target_col, 'High', 'Low'])
+
+    print(y)
+    print(x)
+
+    return x, y
+
+def train_val_test_split(df, target_col, test_ratio):
+    val_ratio = test_ratio / (1 - test_ratio)
+    X, y = feature_label_split(df, target_col)
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=test_ratio, shuffle=False)
+    X_train, X_val, y_train, y_val = train_test_split(X_train, y_train, test_size=val_ratio, shuffle=False)
+    return X_train, X_val, X_test, y_train, y_val, y_test
+
+X_train, X_val, X_test, y_train, y_val, y_test = train_val_test_split(df_features, 'Close', 0.2)
+
+from sklearn.preprocessing import MinMaxScaler, StandardScaler, MaxAbsScaler, RobustScaler
+
+def get_scaler(scaler):
+    scalers = {
+        "minmax": MinMaxScaler,
+        "standard": StandardScaler,
+        "maxabs": MaxAbsScaler,
+        "robust": RobustScaler,
+    }
+    return scalers.get(scaler.lower())()
+
+scaler = get_scaler('minmax')
+X_train_arr = scaler.fit_transform(X_train)
+X_val_arr = scaler.transform(X_val)
+X_test_arr = scaler.transform(X_test)
+
+y_train_arr = scaler.fit_transform(y_train)
+y_val_arr = scaler.transform(y_val)
+y_test_arr = scaler.transform(y_test)
+
+from torch.utils.data import TensorDataset, DataLoader
+
+batch_size = 64
+
+train_features = torch.Tensor(X_train_arr)
+train_targets = torch.Tensor(y_train_arr)
+val_features = torch.Tensor(X_val_arr)
+val_targets = torch.Tensor(y_val_arr)
+test_features = torch.Tensor(X_test_arr)
+test_targets = torch.Tensor(y_test_arr)
+
+train = TensorDataset(train_features, train_targets)
+val = TensorDataset(val_features, val_targets)
+test = TensorDataset(test_features, test_targets)
+
+train_loader = DataLoader(train, batch_size=batch_size, shuffle=False, drop_last=True)
+val_loader = DataLoader(val, batch_size=batch_size, shuffle=False, drop_last=True)
+test_loader = DataLoader(test, batch_size=batch_size, shuffle=False, drop_last=True)
+test_loader_one = DataLoader(test, batch_size=1, shuffle=False, drop_last=True)
+
+class RNNModel(nn.Module):
+    def __init__(self, input_dim, hidden_dim, layer_dim, output_dim, dropout_prob):
+        super(RNNModel, self).__init__()
+
+        # Defining the number of layers and the nodes in each layer
+        self.hidden_dim = hidden_dim
+        self.layer_dim = layer_dim
+
+        # RNN layers
+        self.rnn = nn.RNN(
+            input_dim, hidden_dim, layer_dim, batch_first=True, dropout=dropout_prob
+        )
+        # Fully connected layer
+        self.fc = nn.Linear(hidden_dim, output_dim)
+
+    def forward(self, x):
+        # Initializing hidden state for first input with zeros
+        h0 = torch.zeros(self.layer_dim, x.size(0), self.hidden_dim).requires_grad_()
+
+        # Forward propagation by passing in the input and hidden state into the model
+        out, h0 = self.rnn(x, h0.detach())
+
+        # Reshaping the outputs in the shape of (batch_size, seq_length, hidden_size)
+        # so that it can fit into the fully connected layer
+        out = out[:, -1, :]
+
+        # Convert the final state to our desired output shape (batch_size, output_dim)
+        out = self.fc(out)
+        return out
+
+class LSTMModel(nn.Module):
+    def __init__(self, input_dim, hidden_dim, layer_dim, output_dim, dropout_prob):
+        super(LSTMModel, self).__init__()
+
+        # Defining the number of layers and the nodes in each layer
+        self.hidden_dim = hidden_dim
+        self.layer_dim = layer_dim
+
+        # LSTM layers
+        self.lstm = nn.LSTM(
+            input_dim, hidden_dim, layer_dim, batch_first=True, dropout=dropout_prob
+        )
+
+        # Fully connected layer
+        self.fc = nn.Linear(hidden_dim, output_dim)
+
+    def forward(self, x):
+        # Initializing hidden state for first input with zeros
+        h0 = torch.zeros(self.layer_dim, x.size(0), self.hidden_dim).requires_grad_()
+
+        # Initializing cell state for first input with zeros
+        c0 = torch.zeros(self.layer_dim, x.size(0), self.hidden_dim).requires_grad_()
+
+        # We need to detach as we are doing truncated backpropagation through time (BPTT)
+        # If we don't, we'll backprop all the way to the start even after going through another batch
+        # Forward propagation by passing in the input, hidden state, and cell state into the model
+        out, (hn, cn) = self.lstm(x, (h0.detach(), c0.detach()))
+
+        # Reshaping the outputs in the shape of (batch_size, seq_length, hidden_size)
+        # so that it can fit into the fully connected layer
+        out = out[:, -1, :]
+
+        # Convert the final state to our desired output shape (batch_size, output_dim)
+        out = self.fc(out)
+
+        return out
+
+class GRUModel(nn.Module):
+    def __init__(self, input_dim, hidden_dim, layer_dim, output_dim, dropout_prob):
+        super(GRUModel, self).__init__()
+
+        # Defining the number of layers and the nodes in each layer
+        self.layer_dim = layer_dim
+        self.hidden_dim = hidden_dim
+
+        # GRU layers
+        self.gru = nn.GRU(
+            input_dim, hidden_dim, layer_dim, batch_first=True, dropout=dropout_prob
+        )
+
+        # Fully connected layer
+        self.fc = nn.Linear(hidden_dim, output_dim)
+
+    def forward(self, x):
+        # Initializing hidden state for first input with zeros
+        h0 = torch.zeros(self.layer_dim, x.size(0), self.hidden_dim).requires_grad_()
+
+        # Forward propagation by passing in the input and hidden state into the model
+        out, _ = self.gru(x, h0.detach())
+
+        # Reshaping the outputs in the shape of (batch_size, seq_length, hidden_size)
+        # so that it can fit into the fully connected layer
+        out = out[:, -1, :]
+
+        # Convert the final state to our desired output shape (batch_size, output_dim)
+        out = self.fc(out)
+
+        return out
+
+class Optimization:
+    def __init__(self, model, loss_fn, optimizer):
+        self.model = model
+        self.loss_fn = loss_fn
+        self.optimizer = optimizer
+        self.train_losses = []
+        self.val_losses = []
+
+    def train_step(self, x, y):
+        # Sets model to train mode
+        self.model.train()
+
+        # Makes predictions
+        yhat = self.model(x)
+
+        # Computes loss
+        loss = self.loss_fn(y, yhat)
+
+        # Computes gradients
+        loss.backward()
+
+        # Updates parameters and zeroes gradients
+        self.optimizer.step()
+        self.optimizer.zero_grad()
+
+        # Returns the loss
+        return loss.item()
+
+    def train(self, train_loader, val_loader, batch_size=64, n_epochs=50, n_features=1):
+        model_path = f'models/TSLA'
+
+        for epoch in range(1, n_epochs + 1):
+            batch_losses = []
+            for x_batch, y_batch in train_loader:
+                x_batch = x_batch.view([batch_size, -1, n_features]).to(device)
+                y_batch = y_batch.to(device)
+                loss = self.train_step(x_batch, y_batch)
+                batch_losses.append(loss)
+            training_loss = np.mean(batch_losses)
+            self.train_losses.append(training_loss)
+
+            with torch.no_grad():
+                batch_val_losses = []
+                for x_val, y_val in val_loader:
+                    x_val = x_val.view([batch_size, -1, n_features]).to(device)
+                    y_val = y_val.to(device)
+                    self.model.eval()
+                    yhat = self.model(x_val)
+                    val_loss = self.loss_fn(y_val, yhat).item()
+                    batch_val_losses.append(val_loss)
+                validation_loss = np.mean(batch_val_losses)
+                self.val_losses.append(validation_loss)
+
+            if (epoch <= 10) | (epoch % 50 == 0):
+                print(
+                    f"[{epoch}/{n_epochs}] Training loss: {training_loss:.4f}\t Validation loss: {validation_loss:.4f}"
+                )
+
+        torch.save(self.model.state_dict(), model_path)
+
+    def evaluate(self, test_loader, batch_size=1, n_features=1):
+        with torch.no_grad():
+            predictions = []
+            values = []
+            for x_test, y_test in test_loader:
+                x_test = x_test.view([batch_size, -1, n_features]).to(device)
+                y_test = y_test.to(device)
+                self.model.eval()
+                yhat = self.model(x_test)
+                predictions.append(yhat.to(device).detach().numpy())
+                values.append(y_test.to(device).detach().numpy())
+
+        return predictions, values
+
+    def plot_losses(self):
+        plt.plot(self.train_losses, label="Training loss")
+        plt.plot(self.val_losses, label="Validation loss")
+        plt.legend()
+        plt.title("Losses")
+        plt.show()
+        plt.close()
+
+def get_model(model, model_params):
+    models = {
+        "rnn": RNNModel,
+        "lstm": LSTMModel,
+        "gru": GRUModel,
+    }
+    return models.get(model.lower())(**model_params)
+
+import torch.optim as optim
+
+input_dim = len(X_train.columns)
+output_dim = 1
+hidden_dim = 64
+layer_dim = 3
+batch_size = 64
+dropout = 0.2
+n_epochs = 60
+learning_rate = 1e-3
+weight_decay = 1e-6
+
+model_params = {'input_dim': input_dim,
+                'hidden_dim' : hidden_dim,
+                'layer_dim' : layer_dim,
+                'output_dim' : output_dim,
+                'dropout_prob' : dropout}
+
+model = get_model('gru', model_params)
+
+loss_fn = nn.MSELoss(reduction="mean")
+optimizer = optim.Adam(model.parameters(), lr=learning_rate, weight_decay=weight_decay)
+
+
+opt = Optimization(model=model, loss_fn=loss_fn, optimizer=optimizer)
+opt.train(train_loader, val_loader, batch_size=batch_size, n_epochs=n_epochs, n_features=input_dim)
+opt.plot_losses()
+
+predictions, values = opt.evaluate(
+    test_loader_one,
+    batch_size=1,
+    n_features=input_dim
+)
+
+def inverse_transform(scaler, df, columns):
+    for col in columns:
+        df[col] = scaler.inverse_transform(df[col])
+    return df
+
+
+def format_predictions(predictions, values, df_test, scaler):
+    vals = np.concatenate(values, axis=0).ravel()
+    preds = np.concatenate(predictions, axis=0).ravel()
+    df_result = pd.DataFrame(data={"Close": vals, "prediction": preds}, index=df_test.head(len(vals)).index)
+    df_result = df_result.sort_index()
+    df_result = inverse_transform(scaler, df_result, [["Close", "prediction"]])
+    return df_result
+
+
+df_result = format_predictions(predictions, values, X_test, scaler)
+df_result
+
+from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
+
+def calculate_metrics(df):
+    result_metrics = {'mae' : mean_absolute_error(df['Close'], df.prediction),
+                      'rmse' : mean_squared_error(df['Close'], df.prediction) ** 0.5,
+                      'r2' : r2_score(df['Close'], df.prediction)}
+
+    print("Mean Absolute Error:       ", result_metrics["mae"])
+    print("Root Mean Squared Error:   ", result_metrics["rmse"])
+    print("R^2 Score:                 ", result_metrics["r2"])
+    return result_metrics
+
+result_metrics = calculate_metrics(df_result)
+
+from sklearn.linear_model import LinearRegression
+
+def build_baseline_model(df, test_ratio, target_col):
+    X, y = feature_label_split(df, target_col)
+    X_train, X_test, y_train, y_test = train_test_split(
+        X, y, test_size=test_ratio, shuffle=False
+    )
+    model = LinearRegression()
+    model.fit(X_train, y_train)
+    prediction = model.predict(X_test)
+
+    result = pd.DataFrame(y_test)
+    result["prediction"] = prediction
+    result = result.sort_index()
+
+    return result
+
+df_baseline = build_baseline_model(df_features, 0.2, 'Close')
+baseline_metrics = calculate_metrics(df_baseline)
+import plotly.offline as pyo
+import plotly.graph_objs as go
+from plotly.offline import plot
+
+
+def plot_predictions(df_result, df_baseline):
+    data = []
+
+    value = go.Scatter(
+        x=df_result.index,
+        y=df_result['Close'],
+        mode="lines",
+        name="values",
+        marker=dict(),
+        text=df_result.index,
+        line=dict(color="rgba(0,0,0, 0.3)"),
+    )
+    data.append(value)
+
+    baseline = go.Scatter(
+        x=df_baseline.index,
+        y=df_baseline.prediction,
+        mode="lines",
+        line={"dash": "dot"},
+        name='linear regression',
+        marker=dict(),
+        text=df_baseline.index,
+        opacity=0.8,
+    )
+    data.append(baseline)
+
+    prediction = go.Scatter(
+        x=df_result.index,
+        y=df_result.prediction,
+        mode="lines",
+        line={"dash": "dot"},
+        name='predictions',
+        marker=dict(),
+        text=df_result.index,
+        opacity=0.8,
+    )
+    data.append(prediction)
+
+    layout = dict(
+        title="Predictions vs Actual Values for the dataset",
+        xaxis=dict(title="Time", ticklen=5, zeroline=False),
+        yaxis=dict(title="Close", ticklen=5, zeroline=False),
+    )
+
+    fig = dict(data=data, layout=layout)
+    plot(fig)
+
+plot_predictions(df_result, df_baseline)
